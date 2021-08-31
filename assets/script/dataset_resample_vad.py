2477ea6f3a74e94ca4d9d639af95297174623c70
Kai Wei <ga642381@gmail.com>
2021-09-01 05:01:14 +0800

    seperated vad preprocessing when testing (so that we don't add noise after vad)

--- assets/script/dataset_resample_vad.py
+++ assets/script/dataset_resample_vad.py
@@ -4,11 +4,26 @@ import librosa
 import torch
 import torchaudio
 from tqdm import tqdm
-from utils.noise import WavAug
+from utils.wav2mel import SoxEffects
 from pathlib import Path
 import os
 from functools import partial
 from multiprocessing import Pool, cpu_count
+
+
+"""
+The reason we seperate vad from sox effect and make it an independent script is that
+after adding the noise, vad might not detect human voice then return None. Therefore,
+we would like to apply vad as a preprocessing step and we will not ever do vad again.
+
+The pipeline is :
+wav -> norm -> vad -> add_noise (-> mel, other features)
+
+* Should do norm before vad!
+
+The RULE is:
+DON'T DO VAD AFTER ADDING NOISE
+"""
 
 
 def parse_args():
@@ -20,15 +35,15 @@ def parse_args():
 
 
 def process_save_wav(wav_file, processor, data_dir, save_dir, sr):
-    """
-    We use librosa.load is because resampling with librosa is simpler than torchaudio.load
-    """
     output_path = wav_file.replace(str(data_dir), str(save_dir))
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
 
     wav, _ = librosa.load(wav_file, sr=sr)
-    noisy_wav = processor.add_noise(torch.tensor(wav))
-    torchaudio.save(output_path, noisy_wav, sample_rate=sr)
+    output_wav = processor(torch.tensor(wav).unsqueeze(0), sr)
+    if output_wav is not None:
+        torchaudio.save(output_path, output_wav, sample_rate=sr)
+    else:
+        print(f"[WARNING] vad failed : {wav_file}")
 
 
 def main(data_dir, save_dir, out_sample_rate):
@@ -37,7 +52,7 @@ def main(data_dir, save_dir, out_sample_rate):
     save_dir = Path(save_dir).resolve()
     assert data_dir != save_dir, f"data_dir and save_dir should not be the same!"
     assert data_dir.exists(), f"{data_dir} does not exist!"
-    print(f"[INFO] Task : Adding Noise")
+    print(f"[INFO] Task : resample and vad")
     print(f"[INFO] data_dir : {data_dir}")
     print(f"[INFO] save_dir : {save_dir}")
     save_dir.mkdir(parents=True, exist_ok=True)
@@ -46,20 +61,27 @@ def main(data_dir, save_dir, out_sample_rate):
     wav_files = librosa.util.find_files(data_dir)
     print(f"[INFO] {len(wav_files)} wav files found in {data_dir}")
 
-    # add noise with multiporcessing
-    wavaug = WavAug(sample_rate=out_sample_rate, mode="test")
-    file_to_noisy_file = partial(
+    # add noise
+    sox_effects = SoxEffects(sample_rate=out_sample_rate, norm_db=-3)
+    file_to_processed_file = partial(
         process_save_wav,
-        processor=wavaug,
+        processor=sox_effects,
         data_dir=data_dir,
         save_dir=save_dir,
         sr=out_sample_rate,
     )
+
     N_processes = cpu_count()
     print(f"[INFO] Start multiprocessing with {N_processes} processes")
     with Pool(processes=N_processes) as pool:
-        for _ in tqdm(pool.imap(file_to_noisy_file, wav_files), total=len(wav_files)):
+        for _ in tqdm(
+            pool.imap(file_to_processed_file, wav_files), total=len(wav_files)
+        ):
             pass
+
+    # if you have trouble in multiprocessing, use this :
+    # for wav_file in tqdm(wav_files):
+    #     file_to_processed_file(wav_file)
 
     # copy text files
     txt_in_dir = data_dir / "txt"
---
+++ assets/script/utils/wav2mel.py
@@ -0,0 +1,130 @@
+"""Wav2Mel for processing audio data."""
+
+import torch
+from torchaudio.sox_effects import apply_effects_tensor
+from torchaudio.transforms import MelSpectrogram
+
+
+class Wav2Mel(torch.nn.Module):
+    """Transform audio file into mel spectrogram tensors."""
+
+    def __init__(
+        self,
+        sample_rate: float = 16000,
+        norm_db: float = -3.0,
+        fft_window_ms: float = 50.0,
+        fft_hop_ms: float = 12.5,
+        n_fft: int = 2048,
+        f_min: float = 50.0,
+        n_mels: int = 80,
+        preemph: float = 0.97,
+        ref_db: float = 20.0,
+        dc_db: float = 100.0,
+    ):
+        super().__init__()
+
+        self.sample_rate = sample_rate
+        self.norm_db = norm_db
+        self.fft_window_ms = fft_window_ms
+        self.fft_hop_ms = fft_hop_ms
+        self.n_fft = n_fft
+        self.f_min = f_min
+        self.n_mels = n_mels
+        self.preemph = preemph
+        self.ref_db = ref_db
+        self.dc_db = dc_db
+
+        self.sox_effects = SoxEffects(sample_rate, norm_db)
+        self.log_melspectrogram = LogMelspectrogram(
+            sample_rate,
+            fft_window_ms,
+            fft_hop_ms,
+            n_fft,
+            f_min,
+            n_mels,
+            preemph,
+            ref_db,
+            dc_db,
+        )
+
+    def mel(self, wav_tensor: torch.Tensor) -> torch.Tensor:
+        mel_tensor = self.log_melspectrogram(wav_tensor)
+        return mel_tensor
+
+    def forward(self, wav_tensor: torch.Tensor, sample_rate: int) -> torch.Tensor:
+        # 1. sox effect
+        wav_tensor = self.sox_effects(wav_tensor, sample_rate)
+        if wav_tensor is None:
+            return None
+
+        # 2. log mel-spectrogram
+        mel_tensor = self.log_melspectrogram(wav_tensor)
+        return mel_tensor
+
+
+class SoxEffects(torch.nn.Module):
+    """Transform waveform tensors."""
+
+    def __init__(self, sample_rate: int, norm_db: float):
+        super().__init__()
+        self.effects = [
+            ["channels", "1"],  # convert to mono
+            ["rate", f"{sample_rate}"],  # resample
+            ["norm", f"{norm_db}"],  # normalize to -3 dB
+            ["vad"],
+            ["reverse"],
+            ["vad"],
+            ["reverse"]
+            # remove silence throughout the file
+            # vad only trim beginning of the utternece
+            # use "reverse" to trim the end of the utterence
+        ]
+
+    def forward(self, wav_tensor: torch.Tensor, sample_rate: int) -> torch.Tensor:
+        wav_tensor, _ = apply_effects_tensor(wav_tensor, sample_rate, self.effects)
+        if wav_tensor.numel() == 0:
+            return None
+        return wav_tensor
+
+
+class LogMelspectrogram(torch.nn.Module):
+    """Transform waveform tensors into log mel spectrogram tensors."""
+
+    def __init__(
+        self,
+        sample_rate: float,
+        fft_window_ms: float,
+        fft_hop_ms: float,
+        n_fft: int,
+        f_min: float,
+        n_mels: int,
+        preemph: float,
+        ref_db: float,
+        dc_db: float,
+    ):
+        super().__init__()
+        self.melspectrogram = MelSpectrogram(
+            sample_rate=sample_rate,
+            win_length=int(sample_rate * fft_window_ms / 1000),
+            hop_length=int(sample_rate * fft_hop_ms / 1000),
+            n_fft=n_fft,
+            f_min=f_min,
+            n_mels=n_mels,
+        )
+        self.preemph = preemph
+        self.ref_db = ref_db
+        self.dc_db = dc_db
+
+    def forward(self, wav_tensor: torch.Tensor) -> torch.Tensor:
+        # preemph
+        wav_tensor = torch.cat(
+            (
+                wav_tensor[:, 0].unsqueeze(-1),
+                wav_tensor[:, 1:] - self.preemph * wav_tensor[:, :-1],
+            ),
+            dim=-1,
+        )
+        mel_tensor = self.melspectrogram(wav_tensor).squeeze(0)  # (n_mels, time)
+        mel_tensor = 20 * mel_tensor.clamp(min=1e-9).log10()
+        mel_tensor = (mel_tensor - self.ref_db + self.dc_db) / self.dc_db
+        return mel_tensor
